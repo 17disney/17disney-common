d85f3e81d3ec8bdb49bd78874d9f99ec6923d81f
xank <xank@qq.com>
2018-06-28 19:02:40 +0800

    add tip

--- filters/index.js
+++ filters/index.js
@@ -1,6 +1,6 @@
 import moment from 'moment'
 
-export function timeFormat (value, format, src = 'YYYY-MM-DD') {
+export function timeFormat(value, format, src = 'YYYY-MM-DD') {
   let val = moment(value, src).format(format)
   if (format === 'd') {
     const week = ['日', '一', '二', '三', '四', '五', '六']
